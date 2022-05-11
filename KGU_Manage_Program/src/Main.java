fcb21bac9007ea78d5341c94e5d75b1689d9c632
PotatoKingPotato <102517919+PotatoKingPotato@users.noreply.github.com>
2022-05-11 21:52:56 +0900

    메인 강탈

--- KGU_Manage_Program/src/Main.java
+++ KGU_Manage_Program/src/Main.java
@@ -1,7 +1,49 @@
-import java.util.Scanner;
+
+class Room {
+  int MaxStudent; // 방이 수용 가능한 최대 인원수
+  
+  public Room() {  }
+  
+  public void SetMaxStudent(int MaxStudent) {
+	  	this.MaxStudent = MaxStudent;
+	  }
+
+  public void SetAvailbble() {
+    return ;
+  }
+  public void SetUnavailbble() {
+	return ;
+  }
+}
+
+//-------------↑Room↑-------------------
+
+class MeetingRoom extends Room{
+	
+	String MeetingRoomName;
+	
+	boolean PresentationAvailable;
+	
+	public MeetingRoom(String MeetingRoomName) {
+		this.MeetingRoomName = MeetingRoomName;
+	}
+	
+	public void ShowMax() {
+		System.out.println("강의실 최대 인원수 : " + MaxStudent);
+	}
+	
+	public void ShowRoomName() {
+		System.out.println("강의실 번호 : " + MeetingRoomName);
+	}
+	
+}
+
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		MeetingRoom R = new MeetingRoom("8002A");
+		R.SetMaxStudent(50);
+		R.ShowRoomName(); R.ShowMax();
 	}
+
 }
