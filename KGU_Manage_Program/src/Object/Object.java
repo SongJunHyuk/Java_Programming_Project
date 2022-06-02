3e79334dbdb1690700de8afb9dadee6dafec47d6
coldcow <93986255+coldcow@users.noreply.github.com>
2022-06-02 21:00:18 +0900

    Object 삽입 기능 추가

--- KGU_Manage_Program/src/Object/Object.java
+++ KGU_Manage_Program/src/Object/Object.java
@@ -1,27 +1,102 @@
 package Object;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Object {
     private boolean obAvailable;
     private ArrayList<Object> objectList = new ArrayList<Object>();
+    private String obName;
     private int obQuantityLeft;
     private static int obQuantity;
- 
-    public Object() {
- 
+    
+    public void setobName(String obName) {
+    	this.obName = obName;
     }
+    
+    public void setobQuantity(int obQuantity) {
+        this.obQuantity = obQuantity;
+    }
+    
+    public String getobName() {
+    	return obName;
+    }
+    
+    public Object() {
+        
+    }
+    
+    public void insertObject() throws InterruptedException {
+        while (true) {
+            Object object = new Object();
+            Scanner sc = new Scanner(System.in);
+            
+            System.out.println("물품명 입력");
+            object.setobName(sc.nextLine());
+            System.out.println("수량 입력");
+            object.setobQuantity(sc.nextInt());
+            object.setobAvailable(true);
  
+            // 입력 사항 맞는지 확인
+            System.out.println("물품명 : " + object.getobName());
+            System.out.println("수량 : " + object.getobQuantity());
+            System.out.println("대여가능 : " + object.isobAvailable());
+            
+            System.out.println("입력하신 사항이 모두 맞습니까? 예(Y) 아니오(N)");
+            String confirm = sc.nextLine();
+            sc.close();
+            if (confirm.equalsIgnoreCase("y")) {
+                objectList.add(object);
+                System.out.println("======입력 완료=====");
+                break;
+            } else if (confirm.equalsIgnoreCase("n")) {
+                System.out.println("물품 정보를 새로 입력하세요.");
+            } else {
+                System.out.println("잘못 누르셨습니다. 초기 메뉴로 이동합니다");
+                break; // 초기메뉴로 이동
+            } 
+        }
+    }
+    
+    public void returnObject() {
+    	
+    }
+//    public void initObject() {
+//        objectList.add(new Ball("Soccer Ball", 10));
+//        objectList.add(new Ball("BasketBall", 10));
+//        objectList.add(new Ball("VolleyBall", 10));
+//        
+//        objectList.add(new AirPump(10));
+//        objectList.add(new Table(10));
+//        
+//        objectList.add(new Tent("White", 5));
+//        objectList.add(new Tent("Black", 5));
+//        objectList.add(new Tent("Green", 5));
+//        
+//        objectList.add(new TwoWayRadio(10));
+//        
+//        objectList.add(new LCar(10));
+//        objectList.add(new Battery(10));
+//        
+//        objectList.add(new ChargingCable("USB A", 10));
+//        objectList.add(new ChargingCable("Lightning", 10));
+//        objectList.add(new ChargingCable("USB C", 10));
+//        
+//        objectList.add(new HairIron(10));
+//        
+//        objectList.add(new HairDryer(10));
+//        
+//        objectList.add(new Helmet("S", 10));
+//        objectList.add(new Helmet("M", 10));
+//        objectList.add(new Helmet("L", 10));   
+//    }
+    
     public boolean isobAvailable() {
         return obAvailable;
     }
  
     public void setobAvailable(boolean obAvailable) {
         this.obAvailable = obAvailable;
-    }
-    
-    public void setobQuantity(int obQuantity) {
-        this.obQuantity = obQuantity;
     }
     
     public int getobQuantity() {
@@ -102,6 +177,12 @@ class Books extends Object{
 
 class Ball extends Object{
 	private static String type;
+	private int quantity;
+	
+	Ball(String type, int quantity){
+		this.type = type;
+		this.quantity = quantity;
+	}
 	
 	public String getBallType() {
 		return type;
@@ -113,12 +194,21 @@ class Ball extends Object{
 }
 
 class AirPump extends Object{
+	private int quantity;
 	
+	AirPump(int quantity){
+		this.quantity = quantity;
+	}
 }
 
 class Table extends Object{
 	private static int tableLength;
 	private static int tableWidth;
+	private int quantity;
+	
+	Table(int quantity){
+		this.quantity = quantity;
+	}
 	
 	public int getTableLength() {
 		return tableLength;
@@ -141,6 +231,13 @@ class Tent extends Object{
 	private static int size;
 	private String color;
 	
+	private int quantity;
+	
+	Tent(String color, int quantity){
+		this.color = color;
+		this.quantity = quantity;
+	}
+	
 	public String getTentColor() {
 		return color;
 	}
@@ -151,17 +248,31 @@ class Tent extends Object{
 }
 
 class TwoWayRadio extends Object{
+	private int quantity;
 	
+	TwoWayRadio(int quantity){
+		this.quantity = quantity;
+	}
 }
 
 class LCar extends Object{
+	private int quantity;
 	
+	LCar(int quantity){
+		this.quantity = quantity;
+	}
 }
 
 class Battery extends Object{
 	private static int capacity;
 	private int capacityLeft;
 	private boolean batteryAvailable;
+	
+	private int quantity;
+	
+	Battery(int quantity){
+		this.quantity = quantity;
+	}
 	
 	public boolean batteryCharged() {
 		return batteryAvailable;
@@ -195,6 +306,13 @@ class Battery extends Object{
 class ChargingCable extends Object{
 	String cableType;
 	
+	private int quantity;
+	
+	ChargingCable(String cableType, int quantity){
+		this.cableType = cableType;
+		this.quantity = quantity;
+	}
+	
 	public String getCableType() {
 		return cableType;
 	}
@@ -205,17 +323,31 @@ class ChargingCable extends Object{
 }
 
 class HairIron extends Object{
+private int quantity;
 	
+	HairIron(int quantity){
+		this.quantity = quantity;
+	}
 }
 
 class HairDryer extends Object{
+	private int quantity;
 	
+	HairDryer(int quantity){
+		this.quantity = quantity;
+	}
 }
 
 class Helmet extends Object{
-	private static int size;
+	private static String size;
+	private int quantity;
 	
-	public int getHelmetSize() {
+	Helmet(String size, int quantity){
+		this.size = size;
+		this.quantity = quantity;
+	}
+	
+	public String getHelmetSize() {
 		return size;
 	}
 	
