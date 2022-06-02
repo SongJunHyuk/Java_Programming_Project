ef9236c17b9272c7fb61a9644bad71f1031c8a77
coldcow <93986255+coldcow@users.noreply.github.com>
2022-06-02 16:24:32 +0900

    Object 재수정

--- KGU_Manage_Program/src/Object/Object.java
+++ KGU_Manage_Program/src/Object/Object.java
@@ -37,15 +37,15 @@ public class Object {
     }
 
 	void increaseobQuantityLeft(int obQuantityLeft) {
-		if (obQuantity == obQuantityLeft){
+		if (obQuantity + obQuantityLeft > obQuantityLeft){
 				System.out.println("Cannot increase quantity above the stated quantity");
 				return;
 		}
 		this.obQuantityLeft += obQuantityLeft;
 	}
 	void decreaseobQuantityLeft(int obQuantityLeft) {
-		if (obQuantityLeft == 0){
-				System.out.println("Cannot decrease quantity below the stated quantity");
+		if (obQuantity - obQuantityLeft < 0){
+				System.out.println("Cannot decrease quantity below zero");
 				return;
 		}
 		this.obQuantityLeft -= obQuantityLeft;
@@ -97,60 +97,126 @@ class Books extends Object{
         this.bGenre = bGenre;
     }
  
-//    public boolean isbAvailable() {
-//        return bAvailable;
-//    }
- 
-//    public void setbAvailable(boolean bAvailable) {
-//        this.bAvailable = bAvailable;
-//    }
- 
 }
 
-//
-//class Ball extends Object_Overlapping{
-//	int size;
-//	String name;
-//}
-//
-//class AirPump extends Object_Overlapping{
-//	
-//}
-//
-//class Table extends Object_Overlapping{
-//	int size;
-//}
-//
-//class Tent extends Object_Overlapping{
-//	int size;
-//	String color;
-//}
-//
-//class TwoWayRadio extends Object_Overlapping{
-//	
-//}
-//
-//class LCar extends Object_Overlapping{
-//	
-//}
-//
-//class Battery extends Object_Overlapping{
-//	int capacity;
-//}
-//
-//class ChargingCable extends Object_Overlapping{
-//	String cableType;
-//}
-//
-//class HairIron extends Object_Overlapping{
-//	
-//}
-//
-//class HairDryer extends Object_Overlapping{
-//	
-//}
-//
-//class Helmet extends Object_Overlapping{
-//	int size;
-//}
 
+class Ball extends Object{
+	private static String type;
+	
+	public String getBallType() {
+		return type;
+	}
+	
+	public void setBallType(String type) {
+		this.type = type;
+	}
+}
+
+class AirPump extends Object{
+	
+}
+
+class Table extends Object{
+	private static int tableLength;
+	private static int tableWidth;
+	
+	public int getTableLength() {
+		return tableLength;
+	}
+	
+	public int getTableWidth() {
+		return tableWidth;
+	}
+	
+	public void setTableLength(int tableLength) {
+		this.tableLength = tableLength;
+	}
+	
+	public void setTableWidth(int tableWidth) {
+		this.tableWidth = tableWidth;
+	}
+}
+
+class Tent extends Object{
+	private static int size;
+	private String color;
+	
+	public String getTentColor() {
+		return color;
+	}
+	
+	public void setTentColor(String color) {
+		this.color = color;
+	}
+}
+
+class TwoWayRadio extends Object{
+	
+}
+
+class LCar extends Object{
+	
+}
+
+class Battery extends Object{
+	private static int capacity;
+	private int capacityLeft;
+	private boolean batteryAvailable;
+	
+	public boolean batteryCharged() {
+		return batteryAvailable;
+	}
+	
+	public void setbatteryCharged(boolean batteryAvailable) {
+		this.batteryAvailable = batteryAvailable;
+	}
+	
+	public int getBatteryCapacity() {
+		return capacity;
+	}
+	
+	public void setBatteryCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	public int getBatteryCapacityLeft() {
+		return capacityLeft;
+	}
+	
+	public void setBatteryCapacityLeft(int capacityLeft) {
+		if (capacityLeft < 0 || capacityLeft > 100) {
+			System.out.println("Battery capacity left has to be between 0 and 100");
+			return;
+		}
+		this.capacityLeft = capacityLeft;
+	}
+}
+
+class ChargingCable extends Object{
+	String cableType;
+	
+	public String getCableType() {
+		return cableType;
+	}
+	
+	public void setCableType(String cableType) {
+		this.cableType = cableType;
+	}
+}
+
+class HairIron extends Object{
+	
+}
+
+class HairDryer extends Object{
+	
+}
+
+class Helmet extends Object{
+	private static int size;
+	
+	public int getHelmetSize() {
+		return size;
+	}
+	
+}
